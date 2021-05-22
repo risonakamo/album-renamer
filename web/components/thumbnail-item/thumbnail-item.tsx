8b72a4d6dfc33698445dd6480ad4e466c531b0aa
risonakamo <hmykanae@gmail.com>
2021-05-21 20:41:42 -0400

    thumbnail auto fit

--- web/components/thumbnail-item/thumbnail-item.tsx
+++ web/components/thumbnail-item/thumbnail-item.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React,{useRef,useState} from "react";
+import cx from "classnames";
 
 import "./thumbnail-item.less";
 
@@ -9,9 +10,32 @@ interface ThumbnailItemProps
 
 export default function ThumbnailItem(props:ThumbnailItemProps):JSX.Element
 {
+  const [isWideFit,setWideFit]=useState<boolean>(false);
+  const imgElement=useRef<HTMLImageElement>(null);
+
+  /** auto fit image on image load */
+  function imageLoaded():void
+  {
+    if (imgElement.current!.naturalWidth>imgElement.current!.naturalHeight)
+    {
+      setWideFit(true);
+    }
+
+    else
+    {
+      setWideFit(false);
+    }
+  }
+
+  const imgElementClasses={
+    wide:isWideFit,
+    tall:!isWideFit
+  };
+
   return <div className="thumbnail-item">
     <div className="image-space">
-      <img src={props.data.path} className="wide"/>
+      <img src={props.data.path} className={cx(imgElementClasses)}
+        ref={imgElement} onLoad={imageLoaded}/>
     </div>
     <div className="title-zone">
       <p>{props.data.name}</p>
