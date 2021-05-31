b73475f0790b9e3d975d08760777f04650be77da
risonakamo <hmykanae@gmail.com>
2021-05-31 14:33:52 -0400

    align group rename button to right

--- web/components/button-84/button-84.tsx
+++ web/components/button-84/button-84.tsx
@@ -9,7 +9,7 @@ interface Button84Props
 
 export default function Button84(props:Button84Props):JSX.Element
 {
-  return <div className="button-84">
+  return <div className="button-84" title="Go to Rename Phase">
     <img src="assets/temp_go-rename-group.png"/>
     rename
   </div>;
