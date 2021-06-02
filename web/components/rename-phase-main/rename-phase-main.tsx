02deb0f124a27d5d4de1715e0b1aa54c18f4bc43
risonakamo <hmykanae@gmail.com>
2021-06-02 01:09:20 -0400

    preparing container

--- web/components/rename-phase-main/rename-phase-main.tsx
+++ web/components/rename-phase-main/rename-phase-main.tsx
@@ -15,7 +15,7 @@ export default function RenamePhaseMain(props:RenamePhaseMainProps):JSX.Element
 {
   return <div className="rename-phase-section phase-layout">
     <section className="top-section header-zone">
-      <div className="header-zone-container">
+      <div className="header-zone-container inputs-zone">
         <ButtonTextBox label="BASEPATH" buttonLabel="BROWSE"/>
         <ButtonTextBox label="AUTO-RENAME" buttonLabel="APPLY" className="auto-rename"/>
       </div>
