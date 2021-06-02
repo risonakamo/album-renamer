1059e46bb757a29f2c7bbe33df2e73759aa44c56
risonakamo <hmykanae@gmail.com>
2021-06-01 22:49:25 -0400

    add rename button to rename page

--- web/components/rename-phase-main/rename-phase-main.tsx
+++ web/components/rename-phase-main/rename-phase-main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import ButtonTextBox from "components/button-text-box/button-text-box";
+import Button84 from "components/button-84/button-84";
 
 import "css/phase-layout.less";
 import "./rename-phase-main.less";
@@ -14,8 +15,13 @@ export default function RenamePhaseMain(props:RenamePhaseMainProps):JSX.Element
 {
   return <div className="rename-phase-section phase-layout">
     <section className="top-section header-zone">
-      <ButtonTextBox label="BASEPATH" buttonLabel="BROWSE"/>
-      <ButtonTextBox label="AUTO-RENAME" buttonLabel="APPLY" className="auto-rename"/>
+      <div className="header-zone-container">
+        <ButtonTextBox label="BASEPATH" buttonLabel="BROWSE"/>
+        <ButtonTextBox label="AUTO-RENAME" buttonLabel="APPLY" className="auto-rename"/>
+      </div>
+      <div className="rename-button-zone header-zone-container">
+        <Button84/>
+      </div>
     </section>
 
     <section className="top-section body-zone">
