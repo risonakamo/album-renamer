b504055a8b71612b0f04078f62c2eb00ea6f89a2
risonakamo <hmykanae@gmail.com>
2021-06-02 21:14:07 -0400

    design entry zone main group name input

--- web/components/rename-group/rename-group.tsx
+++ web/components/rename-group/rename-group.tsx
@@ -14,7 +14,7 @@ export default function RenameGroup(props:RenameGroupProps):JSX.Element
 
     </div>
     <div className="entry-zone zone">
-
+      <input className="group-name-input"/>
     </div>
     <div className="thumbnail-zone zone">
 
