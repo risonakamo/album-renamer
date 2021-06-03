a679263de609224f63f951bd95bdb31823aad07a
risonakamo <hmykanae@gmail.com>
2021-06-02 20:34:56 -0400

    window min height/width

--- app/main.ts
+++ app/main.ts
@@ -12,9 +12,11 @@ function mainWindow():void
     new BrowserWindow({
         width:1520,
         height:890,
+        minWidth:650,
+        minHeight:500,
         webPreferences:{
             spellcheck:false
-        }
+        },
     }).loadFile("web/index.html");
 }
 
