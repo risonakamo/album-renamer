1a3753a35354dd05b7223384130a4b0665ae02ac
risonakamo <hmykanae@gmail.com>
2021-06-14 23:53:55 -0400

    rename convert working for now

--- app/rename-service.ts
+++ app/rename-service.ts
@@ -1,9 +1,17 @@
 import {ipcMain} from "electron";
+import _ from "lodash";
+
+import {renameGroupToRenameActions} from "../lib/rename-convert";
 
 export function attachRenameService():void
 {
     // perform rename api. do the rename given the image groups
-    ipcMain.on("request-rename",(event:Electron.IpcMainEvent,args:ImageGroup[])=>{
-        console.log("got rename request");
+    ipcMain.on("request-rename",(event:Electron.IpcMainEvent,args:RenameRequest)=>{
+        var renameActions:RenameAction[]=_.flatMap(args.groups,(x:ImageGroup):RenameAction[]=>{
+            return renameGroupToRenameActions(x,args.basepath,"");
+        });
+
+        console.log(renameActions);
+        console.log();
     });
 }
--- lib/rename-convert.ts
+++ lib/rename-convert.ts
@@ -1,33 +1,24 @@
 import _ from "lodash";
 import {join,extname} from "path";
 
-import {generateRename} from "lib/rename-rule";
+import {generateRename} from "./rename-rule";
 
-// todo: NOT DONE YET
 /** create rename actions for all items of an ImageGroup */
-export function renameGroupToRenameActions(group:ImageGroup,basepath:string,imageRule:string):RenameAction[]
+export function renameGroupToRenameActions(group:ImageGroup,basepath:string,
+    imageRule:string):RenameAction[]
 {
-    // return _.map(group.items,(x:ImageData2):RenameAction=>{
-    //     return {
-    //         src:x.path,
-    //         target
-    //     };
-    // });
-
-    for (var x=0;x<group.items.length;x++)
-    {
-        var a=renameImage(
-            group.items[x].path,
-            basepath,
-            group.name,
-            imageRule,
-            x
-        );
-
-        console.log(a);
-    }
-
-    return [];
+    return _.map(group.items,(x:ImageData2,i:number):RenameAction=>{
+        return {
+            src:x.path,
+            target:renameImage(
+                x.path,
+                basepath,
+                group.name,
+                imageRule,
+                i
+            )
+        };
+    });
 }
 
 /** generate an renamed image name from various components */
